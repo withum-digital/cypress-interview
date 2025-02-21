3f70274041b4cfcacd2235b012371ab456bd7afd
Phillip Demro <phil@demros.com>
2025-02-21 10:02:01 -0500

    Convert the project to use TypeScript

---
+++ cypress/e2e/mojeekNavigationAndSearch.cy.ts
@@ -0,0 +1,21 @@
+// Scenario 1: Validate Image, News links and Search text functionality
+// Description: User has to navigate to the mojeek website, search for text - “How can we contribute to a greener planet?”
+// and look out for this link, across all the pages from search result - https://earth.org/6-ways-to-go-greener-in-food-production/
+
+// Validation Points:
+// Open any browser
+// User navigates to the website - https://www.mojeek.com/
+// Click on Images link and Verify if user is navigated to this URL - Image Search | Mojeek
+// Click on News and Verify if user is navigated to this URL - News - Mojeek
+// In the Search text box, Input - How can we contribute to a greener planet?
+// To look out for this link, across all the pages from search result - https://earth.org/6-ways-to-go-greener-in-food-production/
+
+describe('Mojeek Navigation and Search', () => {
+  it('Should navigate to the homepage', () => {});
+
+  it('Should navigate to the Images section', () => {});
+
+  it('Should navigate to the News section', () => {});
+
+  it('Should perform a search and verify presence of specific link', () => {});
+});
